185f5cc818ffc080af54bb58d48d5778e980a2f7
Mysik <Mishamysik@gmail.com>
2022-04-22 17:30:45 +0300

    change path for home page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.scss';
 import {useEffect} from "react";
 import {useDispatch} from "react-redux";
 import {fetchToken} from "./config/asyncAction";
-import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 
 function App() {
@@ -13,8 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path='/' element ={<HomePage />} />
-          <Route path='/testtask' element={<Navigate to='/' replace />} />
+          <Route path='/testtask' element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
